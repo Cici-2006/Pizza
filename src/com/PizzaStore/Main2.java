9da93cda7d959e55d74ec5204cf6ccd22ef20012
Cici-2006 <3280711378@qq.cn>
2025-02-21 15:18:04 +0800

    Slice-O-Heaven

---
+++ src/com/PizzaStore/Main2.java
@@ -0,0 +1,10 @@
+package com.PizzaStore;
+
+public class Main2 {
+    public static void main(String[] args)throws Exception {
+        PizzaPrograme sliceOHeaven = new PizzaPrograme();
+        sliceOHeaven.takeOder("1234","set meal","30","orange");
+        sliceOHeaven.makePizza("flour and beaf","20","6");
+    }
+
+}
